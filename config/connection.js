7a1a1aad256c242e9d305fe04b3f7c191a5ca307
Armand Guim <armandguim3@gmail.com>
2018-02-10 02:43:36 -0500

    database and partial js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConncetion({
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'burgers_db'
+
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.error("There was an error connecting to the MySQL Database: " + err.stack);
+		return;	
+	}
+	console.log("You have succesfully connected to the MySQL Database as id: " + connection.threadId);
+});
+
+module.exports = connection; 
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale w/ Cheese', true);
